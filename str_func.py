909af0c391cf388d4e1a7c98f14f9f6e2140f638
Сергей Арефьев <Simpson6515@yandex.ru>
2024-01-12 13:40:40 +0500

    added func_2

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_func(line):
+    '''Возвращает строку с заглаными буквами'''
     return line.upper()
+
+def func_2(line):
+    '''Возвращает строку с первой заглавной буквой'''
+    return line.title()
